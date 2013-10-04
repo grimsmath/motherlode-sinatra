f1b6cce305431e9da807e9ce139c25b34857f273
Matt Walston <m@mlw.ac>
2013-10-04 16:01:56 -0400

    Session creation and destruction through user login/logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,32 @@
 class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(username: params[:login][:username])
+
+    if user && user.authenticate(session_params[:password])
+      session[:username] = user.username
+      redirect_to root_url, notice: 'Logged in!'
+    else
+      flash.now.alert = 'Invalid username or password!'
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:username] = nil
+    redirect_to root_url, notice: 'Logged out!'
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  #def set_user
+  #  @user = User.find(params[:id])
+  #end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def session_params
+    params.require(:login).permit(:username, :password)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Motherlode::Application.routes.draw do
   resources :users
+  resources :sessions
 
-  root 'user#index'
+  get 'login' => 'sessions#new'
+  get 'logout' => 'sessions#destroy'
+
+  root 'users#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
