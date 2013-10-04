8ddc96881ec93cb293190247a7ce4d42ef5bab8e
Matt Walston <m@mlw.ac>
2013-10-04 12:14:05 -0400

    Generated rails app without ActiveRecord

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Motherlode::Application.config.secret_key_base = '07070bf9a57385a32d32cf8dda59a2dbf8b049947d046c69135fd17eb4052fdb4d5bccab92a49c3d4177aabfd90e233c4cf115bb79634eadabea57b5eace731d'
