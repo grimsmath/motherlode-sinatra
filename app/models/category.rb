5ab53185d64ffd95eceb9216e8a45073e258ec85
Matt Walston <m@mlw.ac>
2013-10-04 20:22:59 -0400

    Added display of effective admin perms in drilldown cats

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,11 +4,24 @@ class Category
   has_many :children, class_name: 'Category'
   belongs_to :parent, class_name: 'Category'
 
-  has_and_belongs_to_many :users, inverse_of: nil # Prevents storing category refs in User model
+  has_and_belongs_to_many :admins, class_name: 'User', inverse_of: nil # Prevents storing category refs in User model
 
   field :name, type: String
 
   def to_s
     name
+  end
+
+  def primary_admins
+    # TODO: Make this readable
+    admins.empty? && !parent.nil? ? parent.primary_admins : admins
+  end
+
+  def all_admins
+    retarr = admins || []
+    unless parent.nil?
+      retarr << parent.all_admins
+    end
+    retarr
   end
 end
